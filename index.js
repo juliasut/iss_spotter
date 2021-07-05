6fd186e2c59727645aa3604ba5be4e6b3a165d0b
juliasut <juliasutygina@gmail.com>
2021-07-05 07:51:49 -0600

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const { fetchMyIp } = require('./iss');
 
+fetchMyIp((error, ip) => {
+  if (error) {
+    console.log(`It didn't work!`, error);
+    return;
+  }
+  console.log('It worked! Returned IP:', ip);
+});
--- iss.js
+++ iss.js
@@ -1,1 +1,22 @@
 
+const request = require('request');
+
+
+
+const fetchMyIp = function(callback) {
+  request('https://api.org?format=json', (error, responce, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    if (responce.statusCode !== 200) {
+      callback(Error(`Status Code ${responce.statusCode} when fetching IP: ${body}`), null);
+      return;
+    }
+
+  const ip = JSON.parse(body).ip;
+  callback(null, ip);
+  });
+}
+
+module.exports = { fetchMyIp };
