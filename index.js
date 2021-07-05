309ad806e12e5afb28247b0378ce55c4d6f86b28
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-07-05 15:31:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const printPassTimes = function(passTimes) {
   for (const pass of passTimes) {
     const datetime = new Date(0);
     datetime.setUTCSeconds(pass.risetime);
-    const duration = pass.duuration;
+    const duration = pass.duration;
     console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
 };
