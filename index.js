475025355624405baf2ca72a3f7c74b6bbbbe86e
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-07-05 15:32:48 -0600

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
   console.log(`It worked! Returned coordinates:`, coordinates);
 });
 
-const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+//const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
 fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
   if (error) {
     console.log(`It didn't work!`, error);
